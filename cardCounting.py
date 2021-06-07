89a1b78563e536b9e2603a6d15bc1fbc0e450720
CHoang-public <32404336+CHoang-public@users.noreply.github.com>
2021-06-07 15:07:05 -0400

    Update cardCounting.py

--- cardCounting.py
+++ cardCounting.py
@@ -1,117 +1,138 @@
 
-def zenCount():
-    card = input("Press/input anything to start (type 'end' to quit): ")
-    cardSum = 0
+
+def getCountMethod(cVal, mode, currSum):
     
-    print("Allowed values: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A")
-    while card != 'end':
-        card = input("Insert card num or first letter or name: ")
-        card = card.lower()
-        card = card.replace(" ", "")
+    returnValue = currSum
+    
+    if mode == "zen":
+        returnValue = zenCount(cVal, currSum)
+        #print("rv: " + str(returnValue))
+        return returnValue
+
+    elif mode == "HiLo":
+        returnValue = hiLo(cVal, currSum)
+        #print("rv: " + str(returnValue))
+        return returnValue
+
+    elif mode == "ko":
+        returnValue = koCount(cVal, currSum)
+        #print("rv: " + str(returnValue))
+        return returnValue
+
+    #print("rv: " + str(return_value))
+    
+
+
+
+def zenCount( cVal, cardSum ):
+    
+    card = cVal
+    retVal = cardSum
+     #print(card.isalpha())
+
+    if card.isalpha() == False:
+        card = int(card)   
         
-        #print(card.isalpha())
-        if card.isalpha() == False:
-            card = int(card)
-            
-            if card < 4 or card == 7:
-                cardSum += 1
-                print("Card sum/count: " + str(cardSum))
-            elif card >= 4 and card <= 6:
-                cardSum += 2
-                print("Card sum/count: " + str(cardSum))
-            elif card == 10:
-                cardSum += -2
-                print("Card sum/count: " + str(cardSum))
-            elif card == 8 or card == 9: 
-                cardSum += 0
-                print("Card sum/count: " + str(cardSum))
-            else:
-                print("Improper input, nonreal card")
-            print("")
+        if card < 4 or card == 7:
+            retVal = retVal + 1
+            #print("Card sum/count: " + str(cardSum))
+        elif card >= 4 and card <= 6:
+            retVal = retVal + 2
+            #print("Card sum/count: " + str(cardSum))
+        elif card == 10:
+            retVal = retVal - 2
+            #print("Card sum/count: " + str(cardSum))
+        elif card == 8 or card == 9: 
+            retVal = retVal + 0
+            #print("Card sum/count: " + str(cardSum))
         
-        else:
-            if card == "end" or card=="quit":
-                print("Card sum/count: " + str(cardSum))
-                return
-            if card == "a":
-                cardSum += -1
-                print("Card sum/count: " + str(cardSum))
-            elif card == "j" or card == "k" or card == "q":
-                cardSum += -2
-                print("Card sum/count: " + str(cardSum))
-            else:
-                print("Improper input, nonreal card")
-            print("")
-            
+    elif card == "a":
+        retVal = retVal - 1
+        #print("Card sum/count: " + str(cardSum))
+    elif card == "j" or card == "k" or card == "q":
+        retVal = retVal - 2
+        #print("Card sum/count: " + str(cardSum))
+    
+    
+    #print("zen rv: " + str(retVal))
+    return retVal
+
     """""""""""""""""""""""""""""""""
     2  3  4  5  6  7  8  9  10,j,k,q    A
     1, 1, 2, 2, 2, 1, 0 ,0     -2       -1
     """""""""""""""""""""""""""""""""
 
-def hiLo():
-    card = input("Press/input anything to start (type 'end' to quit): ")
-    cardSum = 0
+
+
+def hiLo( cVal, cardSum ):
     
-    print("Allowed values: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A")
-    while card != 'end':
-        card = input("Insert card num or first letter or name: ")
-        card = card.lower()
-        card = card.replace(" ", "")
-        
+    card = cVal
+    
+    retVal = cardSum
+    
         #print(card.isalpha())
-        if card.isalpha() == False:
-            card = int(card)
+    if card.isalpha() == False:
+        card = int(card)
             
-            if card < 7:
-                cardSum += 1
-                print("Card sum/count: " + str(cardSum))
-            elif card < 10 and card > 6:
-                cardSum += 0
-                print("Card sum/count: " + str(cardSum))
-            elif card == 10:
-                cardSum += -1
-                print("Card sum/count: " + str(cardSum))
-            else:
-                print("Improper input, nonreal card")
-            print("")
-        
-        else:
-            if card == "end" or card =="quit":
-                print("Card sum/count: " + str(cardSum))
-                return
-            if card == "a" or card == "j" or card == "k" or card == "q":
-                cardSum += -1
-                print("Card sum/count: " + str(cardSum))
-            else:
-                print("Improper input, nonreal card")
-            print("")
+        if card < 7:
+            retVal += 1
+            #print("Card sum/count: " + str(cardSum))
+        elif card < 10 and card > 6:
+            retVal += 0
+            #print("Card sum/count: " + str(cardSum))
+        elif card == 10:
+            retVal += -1
+            #print("Card sum/count: " + str(cardSum))
+       
+    elif card == "a" or card == "j" or card == "k" or card == "q":
+        retVal += -1
+        #print("Card sum/count: " + str(cardSum))
+    
+    
+    return retVal
     """""""""""""""""""""""""""""""""
     2  3  4  5  6  7  8  9  10,j,k,q, A
     1, 1, 1, 1, 1, 0, 0 ,0     -1
     """""""""""""""""""""""""""""""""
 
+
+def koCount( cVal, cardSum ):
+    card = cVal
+    
+    retVal = cardSum
+    
+        #print(card.isalpha())
+    if card.isalpha() == False:
+        card = int(card)
+            
+        if card <= 7:
+            retVal += 1
+            #print("Card sum/count: " + str(cardSum))
+        elif card == 10:
+            retVal += -1
+            #print("Card sum/count: " + str(cardSum))
+       
+    elif card == "a" or card == "j" or card == "k" or card == "q":
+        retVal += -1
+        #print("Card sum/count: " + str(cardSum))
+    
+    
+    return retVal
+
+
+"""""""""""""""""""""""""""""""""
+    2  3  4  5  6  7  8  9  10,j,k,q, A
+    1, 1, 1, 1, 1, 0, 0 ,0     -1
+    """""""""""""""""""""""""""""""""
+
+
+
 def main():
-    print("Select card strategy: ")
-    print("    1) Hi-Lo")
-    print("    2) Zen Count")
-    strategy = input("Type the number: ")
-    
-    strategy = strategy.replace(" ", "") #remove whitespace
- 
-    
-    while strategy != "end":
-        if strategy == "1":
-            print("starting hiLo")
-            hiLo()
-        elif strategy == "2":
-            print("starting zenCount")
-            zenCount()
-        
-        strategy = input("Use again or end (type 'end' to end): ")
-        strategy = strategy.replace(" ", "")
-        strategy = strategy.lower()
-    
-    quit()
+
+    testcardSum = 0
+    testcardSum += getCountMethod("2", "HiLo", testcardSum)
+    #print("Final: " + str(testcardSum))
+
         
    
 if __name__ == "__main__":
